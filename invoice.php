bef72123bb4bd149f0e6c2185d056185772f0a95
Nizal Abdul Razak <nizal_2022bcse001@nitsri.ac.in>
2025-01-20 09:09:38 +0300

    test: invoice header corrected

--- invoice.php
+++ invoice.php
@@ -8,7 +8,7 @@ $pdo = new PDO('sqlite:databases/journal.db');
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Transactions</title>
+  <title>Invoice</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
   <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
